119da680e70fd793effab15c2b7f06afdcbd1f89
MuMu <piggy925@163.com>
2021-11-24 14:13:40 +0800

    feat：添加XSS攻击处理

--- src/main/java/com/mumu/emos/api/EmosApiApplication.java
+++ src/main/java/com/mumu/emos/api/EmosApiApplication.java
@@ -3,8 +3,10 @@ package com.mumu.emos.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@ServletComponentScan
 @EnableAsync
 @MapperScan("com.mumu.emos.api.db.dao")
 @SpringBootApplication
