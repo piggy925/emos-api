736e9d32785705fb7ff4a0553e0e32adee7930f9
MuMu <piggy925@163.com>
2021-11-24 14:04:48 +0800

    feat：配置异步执行

--- src/main/java/com/mumu/emos/api/EmosApiApplication.java
+++ src/main/java/com/mumu/emos/api/EmosApiApplication.java
@@ -3,7 +3,9 @@ package com.mumu.emos.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @MapperScan("com.mumu.emos.api.db.dao")
 @SpringBootApplication
 public class EmosApiApplication {
