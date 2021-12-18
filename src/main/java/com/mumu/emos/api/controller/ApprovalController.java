b44bc52c3287985624899c3659c813cb73b813b6
MuMu <piggy925@163.com>
2021-12-18 13:26:29 +0800

    feat：完成查询工作流BPMN接口

--- src/main/java/com/mumu/emos/api/controller/ApprovalController.java
+++ src/main/java/com/mumu/emos/api/controller/ApprovalController.java
@@ -3,28 +3,47 @@ package com.mumu.emos.api.controller;
 import cn.dev33.satoken.annotation.SaCheckPermission;
 import cn.dev33.satoken.annotation.SaMode;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONUtil;
 import com.mumu.emos.api.common.util.PageUtils;
 import com.mumu.emos.api.common.util.R;
 import com.mumu.emos.api.controller.form.SearchApprovalContentForm;
 import com.mumu.emos.api.controller.form.SearchTaskByPageForm;
+import com.mumu.emos.api.exception.EmosException;
 import com.mumu.emos.api.service.ApprovalService;
 import com.mumu.emos.api.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 
+@Slf4j
 @RestController
 @RequestMapping("/approval")
 @Tag(name = "ApprovalController", description = "任务审批Web接口")
 public class ApprovalController {
+    @Value("${workflow.url}")
+    private String workflow;
+
+    @Value("${emos.code}")
+    private String code;
+
+    @Value("${emos.tcode}")
+    private String tcode;
+
     @Resource
     private UserService userService;
 
@@ -53,5 +72,36 @@ public class ApprovalController {
         param.put("role", userService.searchUserRoles(userId));
         HashMap content = approvalService.searchApprovalContent(param);
         return R.ok().put("content", content);
+    }
+
+    @GetMapping("/searchApprovalBpmn")
+    @Operation(summary = "获取BPMN")
+    @SaCheckPermission(value = {"WORKFLOW:APPROVAL", "FILE:ARCHIVE"}, mode = SaMode.OR)
+    public void searchApprovalBpmn(String instanceId, HttpServletResponse response) {
+        if (StrUtil.isBlankIfStr(instanceId)) {
+            throw new EmosException("instanceId不能为空");
+        }
+        HashMap param = new HashMap() {{
+            put("instanceId", instanceId);
+            put("code", code);
+            put("tcode", tcode);
+        }};
+        String url = workflow + "/workflow/searchApprovalBpmn";
+        HttpResponse resp = HttpRequest.post(url).header("Content-Type", "application/json").body(JSONUtil.toJsonStr(param)).execute();
+        if (resp.getStatus() == 200) {
+            try (
+                    InputStream in = resp.bodyStream();
+                    BufferedInputStream bin = new BufferedInputStream(in);
+                    ServletOutputStream out = response.getOutputStream();
+                    BufferedOutputStream bout = new BufferedOutputStream(out)
+            ) {
+                IoUtil.copy(bin, bout);
+            } catch (Exception e) {
+                log.error("执行异常");
+            }
+        } else {
+            log.error("获取工作流BPMN失败");
+            throw new EmosException("获取工作流BPMN失败");
+        }
     }
 }
