31c1ec4ee479569e3c8bd9c5f54985874e644d28
MuMu <piggy925@163.com>
2022-01-05 21:28:58 +0800

    feat：配置WebSocket支持

---
+++ src/main/java/com/mumu/emos/api/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.mumu.emos.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author mumu
+ * @date 2022/1/5
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
